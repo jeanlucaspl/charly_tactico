8d6d91c3d1833ddff891282e90fe58c8b897ff83
JeanLucas <34515830+jeanlucaspl@users.noreply.github.com>
2026-03-07 20:13:32 -0300

    Create trigonometria.js

---
+++ trigonometria.js
@@ -0,0 +1,5 @@
+window.DB_TRIGONOMETRIA = {
+  razonesTrigonometricas: [
+    { nivel:"medio", subtema:"Trigonometría", enunciado:"Calcule: $M = \\sen^2(30°) + \\cos^2(30°)$", alternativas:["A) 0","B) 1/2","C) 1","D) 2"], respuesta:"C", solucion:"Por identidad pitagórica, $\\sen^2(x) + \\cos^2(x) = 1$." }
+  ]
+};
